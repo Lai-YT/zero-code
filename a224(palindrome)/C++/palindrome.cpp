f3803cfce570700ecc51212136c752ea6e095cea
Lai-YT <381xvmvbib@gmail.com>
2020-07-14 12:55:49 +0800

    call as a function: can_be_palindrome

--- a224(palindrome)/C++/palindrome.cpp
+++ a224(palindrome)/C++/palindrome.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 
-int main(void) {
-  std::string sentence;
-  while (std::cin >> sentence) {
-    for (auto &word: sentence) {
-      word = toupper(word);
-    }
-    bool palindrome = true;
-    bool mark = false;
-    for (auto &word: sentence) {
-      if((int) word >= 65 && (int) word <= 90) {
-        int num_of_word = 0;
-        for (auto check: sentence) {
-          if (word == check) {
-            num_of_word++;
-          }
+bool can_be_palindrome(std::string sentence) {
+  for (auto &word: sentence) {
+    word = toupper(word);
+  }
+  bool mark = false;
+  for (auto &word: sentence) {
+    if((int) word >= 65 && (int) word <= 90) {
+      int num_of_word = 0;
+      for (auto check: sentence) {
+        if (word == check) {
+          num_of_word++;
         }
-        if (num_of_word % 2 != 0) {
-          if (!mark) {
-            word = 0;
-            mark = true;
-          } else {
-            palindrome = false;
-            break;
-          }
+      }
+      if (num_of_word % 2 != 0) {
+        if (!mark) {
+          word = 0;
+          mark = true;
+        } else {
+          return false;
         }
       }
     }
-    if (palindrome) {
+  }
+  return true;
+}
+
+
+int main(void) {
+  std::string sentence;
+  while (std::cin >> sentence) {
+
+    if (can_be_palindrome(sentence)) {
       std::cout << "yes !" << '\n';
     } else {
       std::cout << "no..." << '\n';
     }
+
   }
 
   return 0;
--- a224(palindrome)/Python/palindrome.py
+++ a224(palindrome)/Python/palindrome.py
@@ -1,23 +1,26 @@
+
+def can_be_palindrome(sentence:str) -> bool:
+    sentence_upper = sentence.upper()
+    sentence_list = []
+    for i in range(len(sentence_upper)):
+        sentence_list.append(sentence_upper[i])
+
+    mark = False
+    for word in sentence_list:
+        ascii = ord(word)
+        if(65 <= ascii <= 90):
+            if(sentence_list.count(word) % 2 != 0):
+                if not mark:
+                    sentence_list[sentence_list.index(word)] = 0
+                    mark = True
+                else:
+                    return False
+    return True
+
 while True:
     try:
-        sentence = input().upper()
-        sentence_list = []
-        for i in range(len(sentence)):
-            sentence_list.append(sentence[i])
-        mark = False
-        palindrome = True
-
-        for word in sentence_list:
-            ascii = ord(word)
-            if(65 <= ascii <= 90):
-                if(sentence_list.count(word) % 2 != 0):
-                    if not mark:
-                        sentence_list[sentence_list.index(word)] = 0
-                        mark = True
-                    else:
-                        palindrome = False
-                        break
-        if palindrome:
+        sentence = input()
+        if can_be_palindrome(sentence):
             print('yes !')
         else:
             print('no...')
