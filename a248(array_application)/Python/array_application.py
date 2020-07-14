45d6998eda210f74b301874bc9279a29137562ba
Lai-YT <381xvmvbib@gmail.com>
2020-07-14 17:44:30 +0800

    d = a // b, code clean up

--- a248(array_application)/Python/array_application.py
+++ a248(array_application)/Python/array_application.py
@@ -8,12 +8,12 @@ while True:
             a = a % b
             a *= 10000000000
             d = (a // b)
-            if len(str(d)) == 10: print(f'{a//b}', end = '')
-            elif len(str(d)) == 9: print(f'0{a//b}', end = '')
-            elif len(str(d)) == 8: print(f'00{a//b}', end = '')
-            elif len(str(d)) == 7: print(f'000{a//b}', end = '')
-            elif len(str(d)) == 6: print(f'0000{a//b}', end = '')
-            elif len(str(d)) == 5: print(f'00000{a//b}', end = '')
+            if len(str(d)) == 10: print(f'{d}', end = '')
+            elif len(str(d)) == 9: print(f'0{d}', end = '')
+            elif len(str(d)) == 8: print(f'00{d}', end = '')
+            elif len(str(d)) == 7: print(f'000{d}', end = '')
+            elif len(str(d)) == 6: print(f'0000{d}', end = '')
+            elif len(str(d)) == 5: print(f'00000{d}', end = '')
             elif len(str(d)) == 4: print(f'000000{d}', end = '')
             elif len(str(d)) == 3: print(f'0000000{d}', end = '')
             elif len(str(d)) == 2: print(f'00000000{d}', end = '')
