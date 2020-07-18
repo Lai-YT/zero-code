e76221ed93eb3f57fec6eb94b976d2296055a0dd
Lai-YT <381xvmvbib@gmail.com>
2020-07-18 11:02:39 +0800

    function optimization

--- x_a013(roman_numerals)/Python/roman_numerals.py
+++ x_a013(roman_numerals)/Python/roman_numerals.py
@@ -1,9 +1,9 @@
-
 roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
               'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
 
 def roman_to_decimal(roman_number: str) -> int:
     deciml_number, i = 0, 0
+    count = 0
     while i < len(roman_number):
         if i + 1 < len(roman_number):
             if roman_number[i] == 'I' and roman_number[i + 1] == 'V' \
@@ -19,30 +19,32 @@ def roman_to_decimal(roman_number: str) -> int:
         i += 1
     return deciml_number
 
-def partial_convert_to_roman(deciml_number: int, numeral: str, roman_number: list) -> int:
-    times = deciml_number // roman_dict[numeral]
-    deciml_number %= roman_dict[numeral]
-    roman_number.append(numeral * times)
-    return deciml_number
+
+def partial_convert_to_roman(convert_pack: list, numeral: str):
+    times = convert_pack[0] // roman_dict[numeral]
+    convert_pack[0] %= roman_dict[numeral]
+    convert_pack[1].append(numeral * times)
 
 def decimal_to_roman(deciml_number: int) -> str:
     if deciml_number == 0:
         return 'ZERO'
     roman_number = []
-    deciml_number = partial_convert_to_roman(deciml_number, 'M', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'CM', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'D', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'CD', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'C', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'XC', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'L', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'XL', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'X', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'IX', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'V', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'IV', roman_number)
-    deciml_number = partial_convert_to_roman(deciml_number, 'I', roman_number)
+    convert_pack = [deciml_number, roman_number]
+    partial_convert_to_roman(convert_pack, 'M')
+    partial_convert_to_roman(convert_pack, 'CM')
+    partial_convert_to_roman(convert_pack, 'D')
+    partial_convert_to_roman(convert_pack, 'CD')
+    partial_convert_to_roman(convert_pack, 'C')
+    partial_convert_to_roman(convert_pack, 'XC')
+    partial_convert_to_roman(convert_pack, 'L')
+    partial_convert_to_roman(convert_pack, 'XL')
+    partial_convert_to_roman(convert_pack, 'X')
+    partial_convert_to_roman(convert_pack, 'IX')
+    partial_convert_to_roman(convert_pack, 'V')
+    partial_convert_to_roman(convert_pack, 'IV')
+    partial_convert_to_roman(convert_pack, 'I')
     return ''.join(roman_number)
+
 
 while True:
     try:
