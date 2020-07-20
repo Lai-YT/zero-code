a9c194a11cf4bba584e92c8c07e4ace3779e40b0
Lai-YT <381xvmvbib@gmail.com>
2020-07-20 15:02:03 +0800

    optimization: better algorithm

--- f072(garden)/C++/garden.cpp
+++ f072(garden)/C++/garden.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+
 int main(int argc, char const *argv[]) {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(0);
@@ -9,26 +9,26 @@ int main(int argc, char const *argv[]) {
     int fields[num_of_fields] = {0};
     for (int &f: fields) std::cin >> f;
     int start = 0, end = 0, flowers = 0;
-    std::vector<int> fence_position;
-    int num_of_fences = 0;
     for (int i = 0; i < num_of_fields; i++) {
       if (fields[i] == 1) {
-        fence_position.push_back(i);
-        num_of_fences++;
+        start = i;
+        break;
       }
     }
-
-    int i = 0;
-    while (i < num_of_fences - 1) {
-      start = fence_position.at(i);
-      end = fence_position.at(i + 1);
-      int j = start + 1;
-      while (j < end) {
-        if (fields[j] == 0 && fields[j + 1] != 9 && fields[j - 1] != 9)
-          flowers++;
-        j++;
+    for (int i = num_of_fields - 1; i >= 0; i--) {
+      if (fields[i] == 1) {
+        end = i;
+        break;
       }
-      i++;
+    }
+    for (int i = start + 1; i < end; i++) {
+      if (fields[i] == 9) {
+        if (fields[i - 1] != 9) fields[i - 1] = 1;
+        if (fields[i + 1] != 9) fields[i + 1] = 1;
+      }
+    }
+    for (int i = start + 1; i < end; i++) {
+      if (fields[i] == 0) flowers++;
     }
     std::cout << flowers << '\n';
   }
