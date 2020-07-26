9ede64330ba199050b7f482712837815192d19cb
Lai-YT <381xvmvbib@gmail.com>
2020-07-26 22:01:10 +0800

    clean code

--- a040(armstrong_numbers)/Python/armstrong_numbers.py
+++ a040(armstrong_numbers)/Python/armstrong_numbers.py
@@ -3,20 +3,14 @@ while True:
         n, m = map(int, input().split())
         if n == 0 or n > m or m > 1000000:
             break
-        count = 0
+        count = False
         for number in range(n, m):
             power = len(str(number))
-            total = 0
-            temp = number # avoid from changing number
-            while temp != 0:
-                total += (temp % 10) ** power
-                temp //= 10
-            if total == number:
-                count += 1
+            if number == sum([int(n) ** power for n in str(number)]):
+                count = True
                 print(number, end = ' ')
-        if count == 0:
+        if not count:
             print('none', end = '')
         print()
-
     except:
         break
