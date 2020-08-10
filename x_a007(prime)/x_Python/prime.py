4c4fb5769cdf4f6ea254f0a32c64c2c434a982ed
Lai-YT <381xvmvbib@gmail.com>
2020-08-10 11:00:57 +0800

    a007 Python TLE

--- x_a007(prime)/x_Python/prime.py
+++ x_a007(prime)/x_Python/prime.py
@@ -1,30 +1,23 @@
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+for n in range(25, int(2147483647 ** 0.5) + 1, 2):
+    if n % 6 == 1 or n % 6 == 5:
+        ceil = int(n ** 0.5);
+        for i in range(5, ceil + 1, 6):
+            if n % i == 0 or n % (i+2) == 0: break
+        else: primes.append(n)
+
 def is_prime(number: int) -> bool:
-    # primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-    #         73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173]
     if number == 2 or number == 3:
         return True
     if number % 6 != 1 and number % 6 != 5:
         return False
-    else:
-        ceil = int(number ** 0.5)
-        # for n in primes:
-        #     if n < ceil:
-        #         if number % n == 0:
-        #             return False
-        for n in range(5, ceil+1, 6):
-            if number % n == 0 or number % (n+2) == 0:
-                return False
+    if number in primes:
         return True
+    return False
 
-def main():
-    while True:
-        try:
-            number = int(input())
-            if is_prime(number):
-                print("質數")
-            else:
-                print("非質數")
-        except:
-            break
-
-main()
+while True:
+    try: number = int(input())
+    except: break
+    if is_prime(number): print("質數")
+    else: print("非質數")
