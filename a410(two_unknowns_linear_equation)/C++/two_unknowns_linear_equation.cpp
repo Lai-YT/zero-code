1f0a4575da143de5fd590e9f3b181601c85a732c
Lai-YT <381xvmvbib@gmail.com>
2021-08-06 16:53:39 +0800

    refactoring

--- a410(two_unknowns_linear_equation)/C++/two_unknowns_linear_equation.cpp
+++ a410(two_unknowns_linear_equation)/C++/two_unknowns_linear_equation.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[]) {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(0);
-  
+
   double a, b, c, d, e, f;
   while (std::cin >> a >> b >> c >> d >> e >> f) {
     double delta = a * e - b * d;
@@ -17,7 +17,8 @@ int main(int argc, char const *argv[]) {
     double delta_y = a * f - c * d;
     double x = delta_x / delta;
     double y = delta_y / delta;
-    std::cout << std::fixed << std::setprecision(2) << "x=" << delta_x / delta << "\ny=" << delta_y / delta << '\n';
+    std::cout << std::fixed << std::setprecision(2)
+              << "x=" << x << "\ny=" << y << '\n';
   }
   return 0;
 }
