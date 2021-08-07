725168171850c2614aa9cd7e2efbc73ca55c4442
Lai-YT <381xvmvbib@gmail.com>
2021-08-07 21:02:45 +0800

    refactoring

--- c680(computer_scoring_1)/Python/computer_scoring.py
+++ c680(computer_scoring_1)/Python/computer_scoring.py
@@ -1,13 +1,17 @@
 from sys import stdin, stdout
-while True:
-    line = stdin.readline()
-    if not line: break
+
+
+for line in stdin:
     num_of_problems = int(line)
-    correct_answer = stdin.readline()
+    correct_answer: str = stdin.readline()
     num_of_students = int(stdin.readline())
-    for student in range(num_of_students):
-        answer = stdin.readline()
-        correct = 0
+
+    # stores the answers in str and compare them directly by indexing
+    for _ in range(num_of_students):
+        answer: str = stdin.readline()
+        correct: int = 0
+        # too much not count, not enough no penalty
         for i in range(num_of_problems if len(answer) > num_of_problems else len(answer)):
-            if answer[i] == correct_answer[i]: correct += 1
+            if answer[i] == correct_answer[i]:
+                correct += 1
         stdout.write(f'{(correct * 100) // num_of_problems}\n')
