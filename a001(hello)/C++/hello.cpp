bc79a870ebbfae20d9c43ad8b43661ee8fc295b8
Lai-YT <381xvmvbib@gmail.com>
2021-08-08 16:54:36 +0800

    refactoring

--- a001(hello)/C++/hello.cpp
+++ a001(hello)/C++/hello.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
 
-int main(void) {
+
+int main(int argc, char const *argv[]) {
   std::string str = "";
-  while(std::cin >> str) {
+  while (std::cin >> str) {
     std::cout << "hello, " + str << '\n';
   }
   return 0;
