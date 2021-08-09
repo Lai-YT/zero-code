b058868270edfe92a96fea95173e9e8325af6b5c
Lai-YT <381xvmvbib@gmail.com>
2021-08-09 10:43:49 +0800

    refactoring

--- a271(colorful_carrot)/C++/colorful_carrot.cpp
+++ a271(colorful_carrot)/C++/colorful_carrot.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+
 
 int main(void) {
   std::ios_base::sync_with_stdio(false);
@@ -6,43 +8,33 @@ int main(void) {
 
   int num_of_cases;
   while (std::cin >> num_of_cases) {
-    int cases[num_of_cases] = {0};
-    for (int i = 0; i < num_of_cases; i++) {
-      std::string day;
+    while (num_of_cases--) {
       int x, y, z, w, n, m;
       std::cin >> x >> y >> z >> w >> n >> m;
       std::cin.ignore();
+      std::string day;
       std::getline(std::cin, day);
       int length = day.length();
-      int j = 0;
-      int state = 0;
-      while (j < length){
-        if (state) {
-          m -= n * state;
-        }
+      int poisons = 0;
+      /* +2 because a day follows by a space */
+      for (int i = 0; i < length; i += 2) {
+        m -= n * poisons;
         if (m <= 0) {
           break;
         }
-        switch (day[j]) {
+        switch (day[i]) {
           case '1': m += x; break;
           case '2': m += y; break;
           case '3': m -= z; break;
           case '4':
             m -= w;
-            state +=1;
+            poisons +=1;
             break;
           case '0': break;
+          default:  break;
         }
-        j += 2;
       }
-      cases[i] = m;
-    }
-    for (int i = 0; i < num_of_cases; i++) {
-      if (cases[i] > 0) {
-        std::cout << cases[i] << "g" << '\n';
-      } else {
-        std::cout << "bye~Rabbit" << '\n';
-      }
+      std::cout << (m > 0 ? std::to_string(m) + "g" : "bye~Rabbit") << '\n';
     }
   }
 
--- a271(colorful_carrot)/Python/colorful_carrot.py
+++ a271(colorful_carrot)/Python/colorful_carrot.py
@@ -1,25 +1,37 @@
+from sys import stdin, stdout
+from typing import Dict, List, Tuple
 
-while True:
-    try:
-        num_of_cases = int(input())
-        case = []
-        for i in range(num_of_cases):
-            x, y, z, w, n, m = map(int, input().split())
-            days = list(map(int, input().split()))
-            state = 0
-            for d in days:
-                if state: m -= n * state
-                if m <= 0: break
-                if d == 1: m += x
-                elif d == 2: m += y
-                elif d == 3: m -= z
-                elif d == 4:
-                    m -= w
-                    state += 1
-                elif d == 0: pass
-            case.append(m)
-        for m in case:
-            if m > 0: print(f'{m}g')
-            else: print('bye~Rabbit')
-    except:
-        break
+
+for line in stdin:
+    for i in range(int(line)):
+        x, y, z, w, n, m = map(int, stdin.readline().split())
+        # index is used as the carrot number,
+        # maps to the corresponding weight increament and poison
+        change: Dict[str, Tuple[int, bool]] = {
+            '0': (0, False), '1': (x, False), '2': (y, False),
+            '3': (-z, False), '4': (-w, True)
+        }
+        # how many time the rabbit is poisoned
+        poisons: int = 0
+        for day in stdin.readline().split():
+            m -= n * poisons
+            if m <= 0:
+                break
+
+            # Not sure why much slower than the start-end section below.
+            # Should've be more efficient since only query once.
+            # Maybe the construction time is more costly than query.
+            # cur_change: Tuple[int, bool] = change[day]
+            # m += cur_change[0]
+            # if cur_change[1]:
+            #     poisons += 1
+
+            # start
+            m += change[day][0]
+            if change[day][1]:
+                poisons += 1
+            # end
+
+        stdout.write(
+            f'{m}g\n' if m > 0 else 'bye~Rabbit\n'
+        )
