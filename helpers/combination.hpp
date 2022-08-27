f490b4350c33269dde74ff5c4861c919902c82e5
Lai-YT <381xvmvbib@gmail.com>
2022-08-27 11:15:16 +0800

    fix: indention

--- helpers/combination.hpp
+++ helpers/combination.hpp
@@ -18,30 +18,30 @@ namespace helpers {
 */
 template<typename T>
 std::vector<std::vector<T>> KCombinations(const std::vector<T>& s, const size_t k) {
-/*
-* Generate a mask array and have the true positions be the positions of the
-* elements to select in each combinations.
-*/
-std::vector<bool> mask(s.size(), true);
-std::fill(mask.begin() + k, mask.end(), false);
+  /*
+  * Generate a mask array and have the true positions be the positions of the
+  * elements to select in each combinations.
+  */
+  std::vector<bool> mask(s.size(), true);
+  std::fill(mask.begin() + k, mask.end(), false);
 
-/*
-* Putting all trues(1s) in the front makes the mask sorted in a descending order,
-* so we use std::prev_permutation to get all possible permutations of mask.
-* And the permutations are indeed "combinations" in respect of the positions.
-*/
-std::vector<std::vector<T>> combinations{};
-/* don't want to count the factorial, but must will it be greater than the size */
-combinations.reserve(s.size());
-do {
-  std::vector<T> comb(k);
-  for (size_t i = 0, j = 0;
-    j < k && i < s.size();  /* shortcut when k elements selected */
-    ++i) {
-      if (mask.at(i)) {
-        /* select */
-        comb.at(j++) = s.at(i);
-      }
+  /*
+  * Putting all trues(1s) in the front makes the mask sorted in a descending order,
+  * so we use std::prev_permutation to get all possible permutations of mask.
+  * And the permutations are indeed "combinations" in respect of the positions.
+  */
+  std::vector<std::vector<T>> combinations{};
+  /* don't want to count the factorial, but must will it be greater than the size */
+  combinations.reserve(s.size());
+  do {
+    std::vector<T> comb(k);
+    for (size_t i = 0, j = 0;
+      j < k && i < s.size();  /* shortcut when k elements selected */
+      ++i) {
+        if (mask.at(i)) {
+          /* select */
+          comb.at(j++) = s.at(i);
+        }
     }
     combinations.push_back(std::move(comb));
   } while (std::prev_permutation(mask.begin(), mask.end()));
